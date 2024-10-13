0331f9931bd8ce9f284d8cd5e985c1e486acc1df
sanghoon <tkdgns5817@gmail.com>
2024-10-14 02:23:39 +0900

    refactor: 코드 수정

--- src/main/java/com/qticket/coupon/infrastructure/kafka/CouponConsumer.java
+++ src/main/java/com/qticket/coupon/infrastructure/kafka/CouponConsumer.java
@@ -4,15 +4,12 @@ import com.qticket.coupon.application.cache.CacheRepository;
 import com.qticket.coupon.application.coupon.dto.request.IssueByAdminRequestDto;
 import com.qticket.coupon.application.coupon.dto.request.IssueByCustomerRequestDto;
 import com.qticket.coupon.application.coupon.exception.AlreadyIssuedUserException;
-import com.qticket.coupon.application.coupon.exception.CouponNotFoundException;
 import com.qticket.coupon.application.coupon.service.CouponService;
 import com.qticket.coupon.application.message.Consumer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -36,7 +33,6 @@ public class CouponConsumer implements Consumer {
         try {
             couponService.IssueByCustomer(Long.parseLong(userId), issueByCustomerRequestDto);
         } catch (AlreadyIssuedUserException e) {
-            UUID couponId = issueByCustomerRequestDto.getCouponId();
             if (cacheRepository.getCouponQuantityById(issueByCustomerRequestDto.getCouponId()) != null) {
                 cacheRepository.increaseCouponQuantity(issueByCustomerRequestDto.getCouponId());
             }
